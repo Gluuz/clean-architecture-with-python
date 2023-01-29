0422304fb60b29155d12cf68846f554d29f22d54
Gluz <brunogluszczukjobs@gmail.com>
2023-01-29 09:57:10 -0300

    feat: create database connection

---
+++ src/infra/config/db_config.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, Engine
+
+
+class DBConnectionHandler:
+    def __init__(self):
+        self.__connection_string = "sqlite://storage.db"
+        self.session = None
+
+    def get_engine_to_database(self) -> Engine:
+
+        connection_engine = create_engine(self.__connection_string)
+        return connection_engine
