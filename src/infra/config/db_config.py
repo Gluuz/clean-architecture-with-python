18006551e7ed875def5596428972cc51e246621c
Gluz <brunogluszczukjobs@gmail.com>
2023-02-04 09:26:14 -0300

    feat: enter and exit database methods

--- src/infra/config/db_config.py
+++ src/infra/config/db_config.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker
 
 
 class DBConnectionHandler:
@@ -10,3 +11,12 @@ class DBConnectionHandler:
 
         connection_engine = create_engine(self.__connection_string)
         return connection_engine
+
+    def __enter__(self):
+        connection_engine = create_engine(self.__connection_string)
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=connection_engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
